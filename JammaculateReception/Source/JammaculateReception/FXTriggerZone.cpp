8b3e917d63eb4455217761d847d5e8cf20603d1a
unknown <reck98@gmail.com>
2023-02-04 21:34:48 -0800

    step 1 of background transition

---
+++ JammaculateReception/Source/JammaculateReception/FXTriggerZone.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FXTriggerZone.h"
+
+// Sets default values
+AFXTriggerZone::AFXTriggerZone()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Collider = CreateDefaultSubobject<UBoxComponent>(TEXT("Collider"));
+	RootComponent = Collider;
+
+	XYOrientation = true;
+	// Size of sprite in pixels times 10
+	XOffset = -31250;
+	YOffset = 25000;
+
+	Transitioning = false;
+
+}
+
+// Called when the game starts or when spawned
+void AFXTriggerZone::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFXTriggerZone::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
